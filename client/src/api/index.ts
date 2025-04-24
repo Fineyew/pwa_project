6e05e2890bcaa9644c5402222e04d8c5ebba23af
Fineyew <capmember.df@gmail.com>
2025-04-23 22:59:15 -0400

    Fix: add ts-node for Railway build

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,19 +1,28 @@
 import axios from 'axios'
 
-let baseURL = ''
+let api = axios.create({ baseURL: '' })
 
+// Load config.json once and configure axios instance
 const configPromise = fetch('/config.json')
-  .then((res) => res.json())
-  .then((config) => {
-    baseURL = config.apiBaseUrl
+  .then(res => res.json())
+  .then(config => {
+    console.log('[CONFIG] Loaded baseURL:', config.apiBaseUrl)
+    api = axios.create({
+      baseURL: config.apiBaseUrl,
+      headers: { 'Content-Type': 'application/json' }
+    })
+  })
+  .catch(err => {
+    console.error('[CONFIG] Failed to load /config.json:', err)
   })
 
+// All exported requests wait for config to load
 export const login = async (username: string, password: string) => {
   await configPromise
-  return axios.post(`${baseURL}/api/login`, { username, password })
+  return api.post('/api/login', { username, password })
 }
 
 export const logout = async (sessionId: string) => {
   await configPromise
-  return axios.post(`${baseURL}/api/logout`, { sessionId })
+  return api.post('/api/logout', { sessionId })
 }
